c1758c7558aa4aca614f7f88aa6afe61574e970d
jj4 <guanz666@qq.com>
2021-01-27 12:04:38 +0800

    Update antlinker.py

--- antlinker.py
+++ antlinker.py
@@ -4,15 +4,16 @@ import requests, json, base64, hashlib
 
 class antlinker(object):
     def __init__(self):
-        self.usr = ' '  # 手机号
-        self.pwd = ' '  # 密码
+        self.usr = ''  # 手机号
+        self.pwd = ''  # 密码
         self.s = requests.Session()
         self.headers = {
-            'User-Agent': 'User-Agent: Dalvik/2.1.0 (Linux; U; Android 11; MI 10 MIUI/21.1.13)',
+            'User-Agent': 'User-Agent: Dalvik/2.1.0 (Linux; U; Android 10; MI 9 MIUI/21.1.13)',
             'Authorization': 'BASIC '
-                             'NTgyYWFhZTU5N2Q1YjE2ZTU4NjhlZjVmOmRiMzU3YmRiNmYzYTBjNzJkYzJkOWM5MjkzMmFkMDYyZWRkZWE5ZjY='
+                             'NTgyYWFhZTU5N2Q1YjE2ZTU4NjhlZjVmOmRiMzU3YmRiNmYzYTBjNzJkYzJkOWM5MjkzMmFkMDYyZWRkZWE5ZjY=',
         }
- 
+        self.user_info = {}
+
     # 登录 获取token
     def get_token(self):
         usr = "{\"LoginModel\":1,\"Service\":\"ANT\",\"UserName\":\"%s\"}" % self.usr
@@ -25,12 +26,10 @@ class antlinker(object):
         login = self.s.post(auth_url, headers=self.headers, data=data)
         token = json.loads(login.text)
         # 获取access token, refresh token
-        # access token 有效期7200s
         access_token = token["access_token"]
-        # refresh token 有效期30days
         refresh_token = token["refresh_token"]
         # 更新headers
-        self.s.headers.update({'AccessToken': 'ACKEY_' + access_token})
+        self.headers['AccessToken'] = 'ACKEY_' + access_token
         # 保存refresh token
         with open("./refresh.token", "w") as f:
             f.write(refresh_token)
@@ -40,7 +39,10 @@ class antlinker(object):
     def verify_token(self, token):
         url = f'https://auth.xiaoyuanjijiehao.com/oauth2/verify?access_token={token}'
         verify = self.s.get(url)
-        if "user_id" in json.loads(verify.text):
+        user_info = json.loads(verify.text)
+        if "user_id" in user_info:
+            # 更新headers
+            self.headers['AccessToken'] = 'ACKEY_' + token
             return True
         else:
             return False
@@ -59,53 +61,106 @@ class antlinker(object):
                 'grant_type': 'refresh_token'
             }
             # 刷新access token
-            response = self.s.post(url, headers=self.headers, data=payload)
-            access_token = json.loads(response.text)["access_token"]
+            refresh = self.s.post(url, headers=self.headers, data=payload)
+            response = json.loads(refresh.text)
+            access_token = response["access_token"]
             # 更新headers
-            self.s.headers.update({'AccessToken': 'ACKEY_' + access_token})
+            self.headers['AccessToken'] = 'ACKEY_' + access_token
+            # 保存access token
             with open("./access.token", "w") as f:
                 f.write(access_token)
         return access_token
 
+    # 查询用户信息
+    def get_user_info(self):
+        url = "https://h5api.xiaoyuanjijiehao.com/api/staff/interface"
+        data = {
+            "Router": "/api/system/myhead",
+            "Method": "POST",
+            "Body": "{}"
+        }
+        user_info = self.s.post(url, headers=self.headers, data=json.dumps(data))
+        response = json.loads(user_info.text)
+        self.user_info["academy"] = response["Data"]["StudentAcademy"]
+        self.user_info["usercode"] = response["Data"]["UserCode"]
+        self.user_info["name"] = response["Data"]["Name"]
+        self.user_info["in_usercode"] = response["Data"]["IntelUserCode"]
+        return True
+
     # 报平安
-    def save_report(self):
+    def safe_report(self):
         url = "https://h5api.xiaoyuanjijiehao.com/api/staff/interface?="
-        # 选择口号、体温、位置
-        data = "{\"Router\":\"/api/studentsafetyreport/report\",\"Method\":\"POST\",\"Body\":\"{" \
-               "\\\"ReportArea\\\":\\\"xx市\\\",\\\"ReportCode\\\":\\\"984b5551-xxxx-xxxx-xxxx-005056bc6061\\\"," \
-               "\\\"UID\\\":\\\"\\\",\\\"Temperature\\\":\\\"1\\\",\\\"ReportAreaLat\\\":\\\"{" \
-               "\\\\\\\"lng\\\\\\\":119.xxxxxxxxxxx,\\\\\\\"lat\\\\\\\":35.xxxxxxxxxxx," \
-               "\\\\\\\"of\\\\\\\":\\\\\\\"inner\\\\\\\"}\\\",\\\"ReportAreaChoiceCode\\\":\\\"\\\"," \
-               "\\\"ReportAreaChoiceName\\\":\\\"xx省xx市xx市\\\"}\"} "
-        # 报平安请求
-        report = self.s.post(url, headers=self.headers, data=data.encode('utf-8'))
-        # 解析json 获取反馈
+        data = {
+            "Router": "/api/studentsafetyreport/report",
+            "Method": "POST",
+            "Body": "{\"ReportArea\":\"xx市\",\"ReportCode\":\"984b5551-4a33-11ea-98a9-005056bc6061\",\"UID\":\"\",\"Temperature\":\"1\",\"ReportAreaLat\":\"{\\\"lng\\\":119.xxxxxxxxxxx,\\\"lat\\\":35.xxxxxxxxxxx,\\\"of\\\":\\\"inner\\\"}\",\"ReportAreaChoiceCode\":\"\",\"ReportAreaChoiceName\":\"xx省xx市xx市\"}"
+        }
+        report = self.s.post(url, headers=self.headers, data=json.dumps(data).encode('utf-8'))
         response = json.loads(report.text)
         try:
-            decoded = response["FeedbackText"]
+            feedback = response["FeedbackText"]
         except Exception as reason:
-            decoded = str(reason) + '错误'
-        feedback = {
-            'text': '校园集结号： ' + decoded
-            # 'desp':
-            }
+            feedback = str(reason)
         return feedback
 
-    # 温度上报
+    # 体温上报
     def upload_temperature(self):
         url = "https://h5api.xiaoyuanjijiehao.com/api/staff/interface"
-        # 校内上报
-        data = "{\"Router\":\"/api/studentncpback/puttemperature\",\"Method\":\"POST\",\"Body\":\"{\\\"user\\\":\\\"24a04c8e-xxxx-xxxx-xxxx-00163e08212a\\\",\\\"temperature\\\":\\\"1\\\",\\\"reportArea\\\":\\\"校内\\\",\\\"memo\\\":\\\"\\\"}\"}"
-        # 校外上报
-        # data = "{\"Router\":\"/api/studentncpback/puttemperature\",\"Method\":\"POST\",\"Body\":\"{\\\"user\\\":\\\"24a04c8e-xxxx-xxxx-xxxx-00163e08212a\\\",\\\"temperature\\\":\\\"1\\\",\\\"reportArea\\\":\\\"xx省xx市xx市\\\",\\\"memo\\\":\\\"\\\"}\"}"
-        upload = self.s.post(url, headers=self.headers, data=data.encode('utf-8'))
+        # 校内
+        data = {
+            "Router": "/api/studentncpback/puttemperature",
+            "Method": "POST",
+            "Body": "{\"user\":\"%s\",\"temperature\":\"1\",\"reportArea\":\"校内\",\"memo\":\"\"}" % self.user_info["in_usercode"]
+        }
+        upload = self.s.post(url, headers=self.headers, data=json.dumps(data).encode('utf-8'))
         response = json.loads(upload.text)
         try:
-            decoded = response["FeedbackText"]
+            feedback = response["FeedbackText"]
         except Exception as reason:
-            decoded = str(reason) + '错误'
-        feedback = {
-            'text': '校园集结号： ' + decoded
-            # 'desp':
-            }
+            feedback = str(reason)
         return feedback
+
+    # 获取 task code
+    def get_task(self):
+        url = "https://h5api.xiaoyuanjijiehao.com/api/staff/interface"
+        data = {
+            "Router": "/api/newcommtask/getstudenttasklist",
+            "Method": "POST",
+            "Body": "{\"UID\":\"\"}"
+        }
+        query = self.s.post(url, headers=self.headers, data=json.dumps(data))
+        response = json.loads(query.text)
+        task_code = response["Data"]["list"][0]["TaskCode"]
+        business_id = response["Data"]["list"][0]["BusinessId"]
+        return task_code, business_id
+
+    # 信息采集
+    def upload_list(self, task_code, template_id):
+        url = "https://h5api.xiaoyuanjijiehao.com/api/staff/interface"
+        data = {
+            "Router": "/api/newcustomerform/submit",
+            "Method": "POST",
+            "Body": "{\"Field\":[{\"FieldCode\":\"disabled\",\"Content\":\"自动化学院\"},{\"FieldCode\":\"disabled\",\"Content\":\"2018206931\"},{\"FieldCode\":\"disabled\",\"Content\":\"管政\"},{\"FieldCode\":\"\",\"Content\":\"< 37.3℃\"},{\"FieldCode\":\"\",\"Content\":\"< 37.3℃\"},{\"FieldCode\":\"\",\"Content\":\"< 37.3℃\"},{\"FieldCode\":\"\",\"Content\":\"校外\"},{\"FieldCode\":\"\",\"Content\":\"370782\"},{\"FieldCode\":\"\",\"Content\":\"否\"},{\"FieldCode\":\"\",\"Content\":\"否\"},{\"FieldCode\":\"\",\"Content\":\"否\"},{\"FieldCode\":\"\",\"Content\":\"否\"},{\"FieldCode\":\"\",\"Content\":\"否\"},{\"FieldCode\":\"\",\"Content\":\"否\"},{\"FieldCode\":\"\",\"Content\":\"否\"},{\"FieldCode\":\"\",\"Content\":\"否\"},{\"FieldCode\":\"\",\"Content\":\"否\"},{\"FieldCode\":\"\",\"Content\":\"否\"}],\"TaskCode\":\"%s\",\"TemplateId\":\"%s\"}" % (task_code, template_id)
+        }
+        upload = self.s.post(url, headers=self.headers, data=json.dumps(data).encode('utf-8'))
+        response = json.loads(upload.text)
+        try:
+            feedback = response["FeedbackText"]
+        except Exception as reason:
+            feedback = str(reason)
+        return feedback
+
+    # 查询填报情况
+    def query_uplist(self, code):
+        url = "https://h5api.xiaoyuanjijiehao.com/api/staff/interface"
+        data = {
+            "Router": "/api/newcommtask/getclasstaskstudentuplist",
+            "Method": "POST",
+            "Body": "{\"TaskCode\":\"%s\",\"Page\":0,\"memo\":\"\"}" % code
+        }
+        query = self.s.post(url, headers=self.headers, data=json.dumps(data))
+        response = json.loads(query.text)
+        title = response["Data"]["detail"]["Title"]
+        content = response["Data"]["list"]
+        content = str(content).replace('UpdateDatetime', 'UpdateTime')
+        return title, content
