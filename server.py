09fbdae83652fb07c05f09339c0849f60bddbae5
jj4 <guanz666@qq.com>
2021-01-27 12:06:59 +0800

    Add in pushplus

--- server.py
+++ server.py
@@ -3,7 +3,23 @@ import requests
 
 
 # Server酱推送
-def server(msg):
-    sckey = 'SCU98925T261aa3ebe1c0465264ccc59ef614f9dc5ec8a774a49d1'  # Server酱sckey
+def server(title, content):
+    sckey = ''  # Server酱sckey
     url = 'http://sc.ftqq.com/%s.send' % sckey
-    requests.post(url, data=msg)
+    body = {
+        'text': title,
+        'desp': content
+    }
+    requests.post(url, data=body)
+
+
+# push plus
+def pushplus(title, content):
+    url = 'http://pushplus.hxtrip.com/send'
+    body = {
+        'token': '',
+        'title': title,
+        'content': content,
+        'template': 'json'
+    }
+    requests.post(url=url, data=body)
