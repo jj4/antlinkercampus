c1c8b1c2e0dff5739c3ec6c46c4adfd414b4432e
jj4 <guanz666@qq.com>
2021-01-27 12:03:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 from antlinker import antlinker
-from server import server
+from server import pushplus
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     test = antlinker()
     try:
         test.refresh_token()
     except:
-        test.get_token()    
+        test.get_token()
+    # 体温上报
+    # temp = test.upload_temperature()
+    # test.query_temperature()
 
-    temp = test.upload_temperature()
-    server(temp)
+    # 今日任务
+    code, bid = test.get_task()
+    # 信息填报
+    feedback = test.upload_list(code, bid)
+    title, content = test.query_uplist(code)
+    pushplus(title + feedback, content)
